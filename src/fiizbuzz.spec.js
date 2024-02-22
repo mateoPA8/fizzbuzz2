0d0fe57cc59fe17818964214b163755f9dfb781e
“mateopoquechoque” <mateo.poquechoque@ucb.edu.bo>
2024-02-22 10:02:05 -0400

    se anadio la funcionalidad de que cuando es el numero 3 devuelva fizz

--- src/fiizbuzz.spec.js
+++ src/fiizbuzz.spec.js
@@ -5,5 +5,11 @@ describe("fizzbuzz", () => {
     expect(generarFizzBuzz(1)).toEqual("1");
   });
   //primero debemos escribir la prueba antes de tocar codigo del archivo js
+  it("deberia generar el mismo numero para otro que no tiene reglas por ejemplo el 2", () => {
+    expect(generarFizzBuzz(2)).toEqual("2");
+  });
+  it("deberia generar fizz para el numero que sigue esa regla como el 3", () => {
+    expect(generarFizzBuzz(3)).toEqual("fizz");
+  });
   
 });
--- src/generarFizzBuzz.js
+++ src/generarFizzBuzz.js
@@ -1,5 +1,10 @@
 function generarFizzBuzz(numero)
 {
+    let FIZZ=3;
+    if(numero==FIZZ)
+    {
+        return "fizz";
+    }
     return numero + "";//para convertir numero a string
 }
 export default generarFizzBuzz;
